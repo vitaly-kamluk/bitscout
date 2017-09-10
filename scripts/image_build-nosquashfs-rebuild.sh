a481b2f871680f44411c6ef1df86c9f9ddfc2a6e
Vitaly Kamluk <username@users.noreply.github.com>
2017-09-10 22:28:09 +0800

    Added UEFI support
    Constructed ISO is now USB/CD and MBR/UEFI hybrid image.

--- scripts/image_build-nosquashfs-rebuild.sh
+++ scripts/image_build-nosquashfs-rebuild.sh
@@ -11,8 +11,10 @@ cd ..
 statusprint "Creating grub-powered image..."
 install_required_package grub-common
 install_required_package grub-pc-bin
+install_required_package grub-efi-ia32
+install_required_package grub-efi-amd64
 install_required_package xorriso
 
-sudo grub-mkrescue -d /usr/lib/grub/i386-pc --modules="iso9660 linux ext2 fshelp ls boot" --output=./$PROJECTNAME-16.04.iso ./image -- -volid "${PROJECTNAME}-${GLOBAL_BUILDID}" -joliet
+sudo grub-mkrescue --modules="iso9660 linux ext2 fshelp ls boot jpeg video_bochs video_cirrus" --output=./$PROJECTNAME-16.04.iso ./image -- -r -volid "${PROJECTNAME}-${GLOBAL_BUILDID}" -partition_offset 16 -J -l -joliet-long -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -no-emul-boot # do not modify volid, it's critical for boot system!
 
 exit 0;
