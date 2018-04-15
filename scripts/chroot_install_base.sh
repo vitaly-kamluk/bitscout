07343385ff97bd62b12e538d5ac0a4999394f252
Vitaly Kamluk <1012671+vitaly-kamluk@users.noreply.github.com>
2018-04-15 11:27:58 +0800

    Added binutils package for all builds.

--- scripts/chroot_install_base.sh
+++ scripts/chroot_install_base.sh
@@ -19,10 +19,10 @@ statusprint "Updating system and installing essential packages.." &&
 if [ $GLOBAL_RELEASESIZE -eq 1 ]
 then
   chroot_exec chroot 'export DEBIAN_FRONTEND=noninteractive &&
-apt-fast -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install   netcat socat casper lupin-casper discover laptop-detect os-prober lxc lxc1 bindfs dialog tmux gawk grub-pc ntpdate  && exit 0 || exit 1' || exit 1
+apt-fast -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install  binutils netcat socat casper lupin-casper discover laptop-detect os-prober lxc lxc1 bindfs dialog tmux gawk grub-pc ntpdate  && exit 0 || exit 1' || exit 1
 else
   chroot_exec chroot 'export DEBIAN_FRONTEND=noninteractive
-apt-fast -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install   file hdparm iptables lshw usbutils parted lsof psmisc strace ltrace time systemd-sysv man-db dosfstools cron busybox-static rsync dmidecode bash-completion command-not-found ntfs-3g netcat socat uuid-runtime vim nano less pv casper lupin-casper discover laptop-detect os-prober lxc lxc1 bindfs wicd-curses dialog tmux gawk ntpdate nbd-server grub-pc   qemu-kvm  && exit 0 || exit 1' || exit 1
+apt-fast -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install  binutils file hdparm iptables lshw usbutils parted lsof psmisc strace ltrace time systemd-sysv man-db dosfstools cron busybox-static rsync dmidecode bash-completion command-not-found ntfs-3g netcat socat uuid-runtime vim nano less pv casper lupin-casper discover laptop-detect os-prober lxc lxc1 bindfs wicd-curses dialog tmux gawk ntpdate nbd-server grub-pc   qemu-kvm  && exit 0 || exit 1' || exit 1
 fi &&
 statusprint "Finished installing packages." &&
 
