5c550322f04a2298349abb012937889081db650c
Xavier Mertens <xavier@rootshell.be>
2020-02-03 18:33:03 +0100

    Added samba (required by qemu to configure SMB shares)

--- scripts/chroot_install_base.sh
+++ scripts/chroot_install_base.sh
@@ -20,11 +20,11 @@ COMMON_PACKAGES="binutils systemd-container netcat socat casper lupin-casper dis
 
 if [ $GLOBAL_RELEASESIZE -eq 1 ]
 then
-  chroot_exec build.$GLOBAL_BASEARCH/chroot "export DEBIAN_FRONTEND=noninteractive &&
+  chroot_exec build.$GLOBAL_BASEARCH/chroot "export DEBIAN_FRONTEND=noninteractive
 apt-fast -y -o Dpkg::Options::=\"--force-confdef\" -o Dpkg::Options::=\"--force-confold\" install $COMMON_PACKAGES  && exit 0 || exit 1" || exit 1
 else
   chroot_exec build.$GLOBAL_BASEARCH/chroot "export DEBIAN_FRONTEND=noninteractive
-apt-fast -y -o Dpkg::Options::=\"--force-confdef\" -o Dpkg::Options::=\"--force-confold\" install $COMMON_PACKAGES file hdparm iptables lshw usbutils parted lsof psmisc strace ltrace time systemd-sysv man-db dosfstools cron busybox-static rsync dmidecode bash-completion command-not-found ntfs-3g uuid-runtime vim nano less pv ifupdown nbd-server grub-pc qemu-kvm fuse libfuse2 python3-fusepy && exit 0 || exit 1" || exit 1
+  apt-fast -y -o Dpkg::Options::=\"--force-confdef\" -o Dpkg::Options::=\"--force-confold\" install $COMMON_PACKAGES file hdparm iptables lshw usbutils parted lsof psmisc strace ltrace time systemd-sysv man-db dosfstools cron busybox-static rsync dmidecode bash-completion command-not-found ntfs-3g uuid-runtime vim nano less pv ifupdown nbd-server grub-pc qemu-kvm fuse libfuse2 python-fusepy samba && exit 0 || exit 1" || exit 1
 fi &&
 
 #statusprint "Installing LXD using snap.."
@@ -35,7 +35,7 @@ fi &&
 statusprint "Finished installing packages." &&
 
 statusprint "Upgrading kbd package." && #kbd is updated separately, because of related GDM issue/bug.
-chroot_exec build.$GLOBAL_BASEARCH/chroot 'export DEBIAN_FRONTEND=noninteractive &&
+chroot_exec build.$GLOBAL_BASEARCH/chroot 'export DEBIAN_FRONTEND=noninteractive
 apt-mark unhold kbd &&
 cp /bin/kbd_mode.dist /bin/kbd_mode &&
 apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" upgrade' || exit 1 &&
