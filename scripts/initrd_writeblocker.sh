d65e8f1e167ad510bf019659091b6053234416fb
Vitaly Kamluk <1012671+vitaly-kamluk@users.noreply.github.com>
2022-04-12 09:37:25 +0000

    Fixed writeblocker, added nmtui

--- scripts/initrd_writeblocker.sh
+++ scripts/initrd_writeblocker.sh
@@ -18,6 +18,11 @@ sudo cp -v ./resources/usr/sbin/{wrtblk,wrtblk-disable,wrtblk-ioerr} ./build.$GL
 if [ $GLOBAL_TARGET = "iso" ]; then
   statusprint "Installing write-blocker to casper system.."
   sudo cp -v ./resources/initrd/scripts/casper-bottom/01wrtblk_all ./build.$GLOBAL_BASEARCH/initrd/main/scripts/casper-bottom/
+  if ! grep "/scripts/casper-bottom/01wrtblk_all" ./build.$GLOBAL_BASEARCH/initrd/main/scripts/casper-bottom/ORDER; then
+    sudo mv ./build.$GLOBAL_BASEARCH/initrd/main/scripts/casper-bottom/ORDER ./build.$GLOBAL_BASEARCH/initrd/main/scripts/casper-bottom/ORDER.old &&
+    ( echo -e '/scripts/casper-bottom/01wrtblk_all "$@"\n[ -e /conf/param.conf ] && . /conf/param.conf'; cat ./build.$GLOBAL_BASEARCH/initrd/main/scripts/casper-bottom/ORDER.old ) | sudo tee ./build.$GLOBAL_BASEARCH/initrd/main/scripts/casper-bottom/ORDER > /dev/null &&
+    sudo rm ./build.$GLOBAL_BASEARCH/initrd/main/scripts/casper-bottom/ORDER.old
+  fi
 else
   statusprint "Installing write-blocker to doublesword system.."
   [ ! -d "./build.$GLOBAL_BASEARCH/initrd/main/scripts/doublesword-bottom/" ] && sudo mkdir -p ./build.$GLOBAL_BASEARCH/initrd/main/scripts/doublesword-bottom/ 
