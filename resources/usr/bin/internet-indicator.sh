aee9f5ad986bb82760af4c2eb5d235939b69b6dc
frentzen2003 <frentzennavaro@gmail.com>
2023-02-26 02:46:01 +0800

    List of improved commits
    File: /resources/usr/bin/internet-indicator.sh
    
    	1. Fixed the failed status by adding "return 0" when it is start or stop
    
    File: /resources/usr/bin/scout-manage
    
    	1. Fixed and improve exports function
    	2. Fixed and improve memwatch dog function
    
    File: /resources/sbin/memwatchdog.sh
    
    	1. Restore the whole code into previous version
    
    Side Note: Main Menu is not arrange properly "introduction" is in the lowest level

--- resources/usr/bin/internet-indicator.sh
+++ resources/usr/bin/internet-indicator.sh
@@ -6,16 +6,18 @@ function startIndicator() {
     if [ $status == 0 ] ; then
         [[ ! -f /run/internetOn.status && ! -f /run/internetOff.status ]] && touch /run/internetOn.status
         [ -f /run/internetOff.status ] && mv /run/internetOff.status /run/internetOn.status
+        return 0;
     else
         [[ ! -f /run/internetOn.status && ! -f /run/internetOff.status ]] && touch /run/internetOff.status
         [ -f /run/internetOn.status ] && mv /run/internetOn.status /run/internetOff.status
+        return 0;
     fi
 
 }
 
 function stopIndicator() {
-    [ -f /run/internetOn.status ] && rm /run/internetOn.status
-    [ -f /run/internetOff.status ] && rm /run/internetOff.status
+    [ -f /run/internetOn.status ] && rm /run/internetOn.status && return 0;
+    [ -f /run/internetOff.status ] && rm /run/internetOff.status && return 0;
 }
 
 case "$1" in
